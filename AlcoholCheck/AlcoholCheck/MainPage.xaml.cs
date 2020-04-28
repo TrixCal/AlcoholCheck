d6a4f0efd880a50e13a67691ed30389f74d5014b
Trayton Larson <traytonlarson01@gmail.com>
2020-04-28 09:05:27 -0500

    Add project files.

---
+++ AlcoholCheck/AlcoholCheck/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AlcoholCheck
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Inp_birthday.Date = DateTime.Now;
+        }
+        void Btn_calculate(object sender, System.EventArgs e)
+        {
+            TimeSpan timeSpan = DateTime.Now - Inp_birthday.Date;
+            if(timeSpan.Days >= 7665)
+            {
+                Lbl_verify.Text = "You can legally drink alcohol, be responsible.";
+            }
+            else 
+            {
+                Lbl_verify.Text = $"You still have {7665 - timeSpan.Days} days until you can drink.";
+            }
+
+        }
+    }
+}
